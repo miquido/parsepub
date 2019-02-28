bdc967e3ae423deebb60d5bc7579ec884da9554c
Kamil Krzysztof <kamil.krzysztof@miquido.com>
2019-02-28 10:26:05 +0100

    [feature/mapping_epub_file_to_the_base_model] Added unit test to check if decompressor not return directories

--- src/main/kotlin/com/miquido/parsepub/Main.kt
+++ src/main/kotlin/com/miquido/parsepub/Main.kt
@@ -1,12 +1,10 @@
 package com.miquido.parsepub
 
-import com.miquido.parsepub.epubparser.EpubParser
 import com.miquido.parsepub.di.parserModule
 import org.koin.standalone.StandAloneContext.startKoin
 
 fun main() {
     startKoin(diModules)
-    EpubParser().parse("test_ebook.epub", "epub_folder")
     System.exit(0)
 }
 
--- src/test/kotlin/com/miquido/parsepub/metadata/EpubDecompressorTest.kt
+++ src/test/kotlin/com/miquido/parsepub/metadata/EpubDecompressorTest.kt
@@ -3,20 +3,25 @@ package com.miquido.parsepub.metadata
 import com.miquido.parsepub.internal.EpubDecompressor
 import org.junit.Before
 import org.junit.Test
+import java.util.zip.ZipEntry
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class EpubDecompressorTest {
 
     private var decompressor: EpubDecompressor? = null
+    private var entities: List<ZipEntry> = listOf()
 
     @Before
     fun setup() {
         decompressor = EpubDecompressor()
+        entities = decompressor?.decompress(EPUB_FILE_PATH, EPUB_DECOMPRESS_PATH) ?: listOf()
     }
 
     @Test
-    fun `decompressor should return correct files count`() {
-        assertEquals(EXPECTED_FILES_COUNT, decompressor?.decompress(EPUB_FILE_PATH, EPUB_DECOMPRESS_PATH)?.size, "Files count differs then expected")
+    fun `decompressor should return correct files count without any directories`() {
+        assertEquals(EXPECTED_FILES_COUNT, entities.size, "Files count differs then expected")
+        assertTrue(entities.none { it.isDirectory }, "Decompressor returns a list of files with directories")
     }
 
     companion object {
