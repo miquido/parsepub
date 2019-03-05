b0862662118eecc69976fcacf013d1793fbec5e6
Marcin Michałek <marcin.michalek@miquido.com>
2019-03-05 13:26:06 +0100

    Add sample Android aplication using parsepub via local maven repo

---
+++ sample/app/src/main/java/com/miquido/parsepubsample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.miquido.parsepubsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.miquido.parsepub.epubparser.EpubParser
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val parser = EpubParser()
+    }
+}
