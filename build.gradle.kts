fedcd38f937c6041235f61176b5d499b3ba44271
Kamil Krzysztof <kamil.krzysztof@miquido.com>
2019-05-20 06:25:10 +0200

    bintray configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,26 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val versionReleaseDate = "2019-05-20T13:00:00.000+0200"
+val versionUserOrgName = "miquido"
+val versionName = "parsepub"
+val versionRepoName = "maven-repo"
+val versionArtifactId = "parsepub"
+val versionNumber = "1.0.0"
+val versionGroupId = "com.miquido"
+val versionPublicationName = "maven"
+val versionPublicationDesc = "epub parser library"
+val versionSiteUrl = "https://www.miquido.com"
+val versionVcsUrl = "https://github.com/miquido/parsepub"
+val versionGithubRepo = "miquido/parsepub"
+val versionLicenseName = "The Apache Software License, Version 2.0"
+val versionLicenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+val versionLabel = arrayOf("epub", "parser", "kotlin")
+val versionAllLicenses = arrayOf("Apache-2.0")
+
+val bintrayUser = (project.properties["BINTRAY_USER"] as String)
+val bintrayApiKey = (project.properties["BINTRAY_API_KEY"] as String)
 
 buildscript {
     repositories {
@@ -15,11 +36,57 @@ plugins {
     application
     kotlin("jvm") version "1.3.20"
     maven
+    `maven-publish`
     id("org.jetbrains.dokka") version "0.9.17"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
-group = "com.miquido"
-version = "1.0-SNAPSHOT"
+publishing {
+    publications {
+        create<MavenPublication>(versionPublicationName) {
+            groupId = versionGroupId
+            artifactId = versionArtifactId
+            version = versionNumber
+
+            from(components["java"])
+
+            pom.withXml({
+                asNode().apply {
+                    appendNode("description", versionPublicationDesc)
+                    appendNode("name", versionName)
+                    appendNode("url", versionSiteUrl)
+                }
+            })
+
+        }
+
+    }
+}
+
+fun findProperty(s: String) = project.findProperty(s) as String?
+
+bintray {
+    user = bintrayUser
+    key = bintrayApiKey
+    setPublications(versionPublicationName)
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        userOrg = versionUserOrgName
+        repo = versionRepoName
+        websiteUrl = versionSiteUrl
+        vcsUrl = versionVcsUrl
+        name = versionArtifactId
+        githubRepo = versionGithubRepo
+        description = versionPublicationDesc
+        desc = versionPublicationDesc
+        setLabels(*versionLabel)
+        setLicenses(*versionAllLicenses)
+        version.apply {
+            name = versionNumber
+            desc = versionPublicationDesc
+            released = versionReleaseDate
+        }
+    })
+}
 
 repositories {
     mavenCentral()
